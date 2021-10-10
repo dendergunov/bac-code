9d45b641882c524b34d44aa46a8816191b811782
dWX871621 <dergunov.denis@huawei.com>
2021-10-10 20:52:37 +0300

    Add command argument for attack analyzer, proceeding analysis with attack estimation

--- bola-analyzer/main.py
+++ bola-analyzer/main.py
@@ -1,24 +1,30 @@
-import annotator
 import argparse
 import os
+
+import annotator
+import attack_analyzer
 
 
 def main():
     parser = argparse.ArgumentParser(description='''OpenAPI specification analyzer and processor that annotates the 
     specification with BOLA properties''')
     parser.add_argument('spec_file', help='''Path to OpenAPI 3.0 specification file''')
-    parser.add_argument('-d', '--dest', dest='savepath', help='''Processed YAML file destination,
-    bola.yaml if not provided''')
+    parser.add_argument('--pd', '--properties-dest', dest='properties_savepath', default='bola_properties.yml',
+                        help='''Annotated YAML spec file destination, bola_properties.yml if not provided''')
+    parser.add_argument('--ad', '--attack-dest', dest='attacks_savepath', default='bola_attacks.yml',
+                        help='''Proposed attacks YAML spec file destination, bola_attacks.yml if not provided''')
     args = parser.parse_args()
     filepath = os.path.join(os.getcwd(), args.spec_file)
     if os.path.isfile(filepath) is not True:
         raise FileNotFoundError("Specified input file does not exist")
-    analyzer = annotator.OpenAPISpecAnnotator()
-    analyzer.parse_spec(filepath)
+    property_analyzer = annotator.OpenAPISpecAnnotator()
+    property_analyzer.parse_spec(filepath)
+    property_analyzer.save_spec(args.properties_savepath)
 
-    if args.savepath is None:
-        args.savepath = 'bola.yaml'
-    analyzer.save_spec(args.savepath)
+    attack_analyzer_instance = attack_analyzer.AttackAnalyzer()
+    attack_analyzer_instance.estimate_attacks(args.properties_savepath)
+    attack_analyzer_instance.save_output(args.attacks_savepath)
+
 
 if __name__ == "__main__":
     main()
