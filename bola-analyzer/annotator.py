19272876d7ef22af7718ccacda17ba5c7b94de83
dWX871621 <dergunov.denis@huawei.com>
2021-11-07 18:40:43 +0300

    Add processed paths counter, authorization description propagation

--- bola-analyzer/annotator.py
+++ bola-analyzer/annotator.py
@@ -12,6 +12,8 @@ class OpenAPISpecAnnotator:
             1: 'Single',
             len(self.operations): 'All',
         }
+        self.paths_found = 0
+        self.authorization_specified = False
 
     def parse_spec(self, filename):
         """Public method to invoke OpenAPI 3.0 specification processing to annotate with BOLA/IDOR properties.
@@ -25,12 +27,17 @@ class OpenAPISpecAnnotator:
         self.spec = yaml.load(f, Loader=yaml.SafeLoader)
         self.bola_spec = self.spec.copy()
 
+        if self.spec.get('security') is not None:
+            self.authorization_specified = True if len(self.spec['security']) > 0 else False
+
         if self.spec.get('paths') is not None:
             print("Paths OpenAPI object found!")
             for path, path_schema in self.spec.get('paths').items():
                 if path.startswith('/'):
                     print("Endpoint found:", path)
                     self.__parse_endpoint(path, path_schema)
+                    self.paths_found += 1
+        print("Total paths processed:", self.paths_found)
 
     def __parse_endpoint(self, path, path_schema):
         path_schema: dict
@@ -39,11 +46,15 @@ class OpenAPISpecAnnotator:
             modified_parameters = self.__analyze_parameters(path_schema.get('parameters'))
             path_schema['parameters'] = modified_parameters
 
+        endpoint_authorization = self.authorization_specified
+        if path_schema.get('security') is not None:
+            endpoint_authorization = True if len(path_schema['security']) > 0 else False
+
         found_operations = [operation for operation in self.operations if path_schema.get(operation)]
         # Method level parameter complement
         for operation in found_operations:
             modified_operation = self.__analyze_operation(operation, path_schema.get(operation),
-                                                          path_schema.get('parameters'))
+                                                          path_schema.get('parameters'), endpoint_authorization)
             path_schema[operation] = modified_operation
 
         # Endpoint level properties complement
@@ -51,7 +62,6 @@ class OpenAPISpecAnnotator:
         endpoint_level_properties = {
             'defined_http_verbs': self.defined_verbs_property_options.get(len(found_operations),
                                                                           'Multiple')}
-
         path_schema['endpoint_level_properties'] = endpoint_level_properties
         self.bola_spec['paths'][path] = path_schema
 
@@ -95,21 +105,23 @@ class OpenAPISpecAnnotator:
                 # ToDo: add checks for personal information type
                 # ToDo: add parsing of complex objects
         parameter_level_properties['is_identifier'] = is_identifier
+
         if is_identifier:
             parameter_level_properties['location'] = parameter_schema['in']
             parameter_level_properties['type'] = parameter_type if parameter_type is not None else schema_field['type']
         return parameter_level_properties
 
-    def __analyze_operation(self, operation, operation_schema, endpoint_parameters=None):
-        # ToDo: change method_level_properties from list to dictionary type
+    def __analyze_operation(self, operation, operation_schema, endpoint_parameters=None, endpoint_authorization=False):
         method_level_properties = {}
         # Operation parameters
         operation_parameters_defined = 'non-empty' if operation_schema.get('parameters') is not None else 'empty'
         method_level_properties['operation_parameters_list'] = operation_parameters_defined
+
         # Annotate operation parameters (do it first may be)
         if operation_schema.get('parameters') is not None:
             modified_parameters = self.__analyze_parameters(operation_schema.get('parameters'))
             operation_schema['parameters'] = modified_parameters
+
         # Number of identifiers targeted/affected property:
         # Make a check for endpoint + operation parameters which one are identifiers
         operation_identifiers_count = 0
@@ -127,12 +139,12 @@ class OpenAPISpecAnnotator:
         method_level_properties['identifiers_used'] = {0: 'zero',
                                                        1: 'single'}.get(operation_identifiers_count, 'multiple')
         # Authorization required check.
-        # ToDo: implement propagation of security fields from endpoint and specification level
         operation_schema: dict
-        authorization_required = False
+        authorization_required = endpoint_authorization
         if operation_schema.get('security') is not None:
             authorization_required = True if len(operation_schema.get('security')) > 0 else False
         method_level_properties['authorization_required'] = authorization_required
+
         operation_schema['method_level_properties'] = method_level_properties
         return operation_schema
 
